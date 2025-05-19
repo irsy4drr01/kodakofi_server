0f664298760eeab47b44723ab302cde702c1c448
Irsyad Ridho <irsyadridho.r@gmail.com>
2025-05-20 02:31:06 +0700

    feat(order): add order routes for GET order details

--- internal/routes/main.routes.go
+++ internal/routes/main.routes.go
@@ -12,7 +12,6 @@ func InitRouter(db *pgxpool.Pool) *gin.Engine {
 	router.Use(middlewares.CORSMiddleware)
 	rg := router.Group("/api")
 	auth(rg, db)
-	// orders(rg, db)
 	profile(rg, db, middlewares)
 	product(rg, db, middlewares)
 	order(rg, db, middlewares)
--- internal/routes/order.routes.go
+++ internal/routes/order.routes.go
@@ -13,6 +13,6 @@ func order(r *gin.RouterGroup, db *pgxpool.Pool, mdw *middlewares.Middleware) {
 	repo := repositories.NewOrder(db)
 	handlers := handlers.NewOrder(repo)
 	route.POST("", mdw.VerifyToken, mdw.AccsessGate("user"), handlers.PostOrderHandler)
-	route.GET("",mdw.VerifyToken, mdw.AccsessGate("user"), handlers.GetHistoryOrders)
+	route.GET("", mdw.VerifyToken, mdw.AccsessGate("user"), handlers.GetHistoryOrders)
+	route.GET("/:order_id", mdw.VerifyToken, mdw.AccsessGate("user"), handlers.FetchDetailOrderByUser)
 }
-
