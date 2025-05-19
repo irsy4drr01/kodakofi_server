668a5749eba0bdff1e83425e48d562acf2308042
Irsyad Ridho <irsyadridho.r@gmail.com>
2025-05-20 02:32:14 +0700

    feat(migrate): update constrain in code_ote table

--- migration/20250515153433_code_otp.up.sql
+++ migration/20250515153433_code_otp.up.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS code_otp (
-  id SERIAL PRIMARY KEY,
-  user_id UUID NOT NULL,
-  code VARCHAR(6) NOT NULL,
+  id SERIAL,
+  user_id UUID NOT NULL PRIMARY KEY,
+  code VARCHAR(6) NOT NULL UNIQUE,
   type_id INT NOT NULL,
   expired_at TIMESTAMPTZ NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
   updated_at TIMESTAMPTZ,
   CONSTRAINT fk_code_otp_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  CONSTRAINT fk_code_otp_type FOREIGN KEY (type_id) REFERENCES otp_type(id) ON DELETE CASCADE,
-  CONSTRAINT unique_user_type UNIQUE (user_id, type_id)
+  CONSTRAINT fk_code_otp_type FOREIGN KEY (type_id) REFERENCES otp_type(id) ON DELETE CASCADE  
 );
